7d487793d606f50b3a66a59ab4a67456b2f575dd
Ville <ville.einio@gmail.com>
2024-10-24 17:17:10 +0300

    Add: switch and exit commands

--- src/radio.py
+++ src/radio.py
@@ -1,4 +1,28 @@
-from scraper.webscraper import WebRadioScraper
+from player.radioplayer import RadioPlayer
+from tools.config_browser import ConfigBrowser
+import argparse
 
-WebRadioScraper.refreshM3U8Urls()
-print("Hello World")
+parser = argparse.ArgumentParser(
+    description="This function is used to interact with the radioplayer"
+)
+
+parser.add_argument("--start", action="store_true")
+parser.add_argument("--default-channel", type=str)
+parser.add_argument("--exit", action="store_true")
+parser.add_argument("--switch", type=str)
+
+args = parser.parse_args()
+
+if args.start:
+    player = RadioPlayer(data_port=ConfigBrowser.getConfig()["default port"])
+    player.start()
+
+if args.default_channel:
+    RadioPlayer.set_default_channel(args.default_channel)
+
+if args.exit:
+    RadioPlayer.send_command(data_port=ConfigBrowser.getConfig()["default port"], command="stop")
+
+if args.switch:
+    channel = args.switch
+    RadioPlayer.send_command(data_port=ConfigBrowser.getConfig()["default port"], command=f"switch {channel}")
